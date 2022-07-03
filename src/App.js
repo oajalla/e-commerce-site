6851524b73ce8f69c0710d76d8a6ce3ddc174679
Oscar Ajalla <oscar.ajalla@outlook.com>
2022-07-03 22:03:59 +0800

    adding styling and navigation folders

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import {Routes, Route} from 'react-router-dom';
-
+import Navigation from './routes/navigation/navigation.component';
 import Home from './routes/home/home.component';
+
+const Shop = () => {
+  return (
+    <div>
+      <h1>this is the Shop page</h1>
+    </div>
+  )
+}
+
 
 const App = () => {
   return(
     <Routes>
-      <Route path='/' element = {<Home/>}/>
+      <Route path='/' element = {<Navigation/>}>
+        <Route index element = {<Home/>}/>
+        <Route path='shop' element = {<Shop/>}/>
+      </Route>
+     
     </Routes>
   );
 };
